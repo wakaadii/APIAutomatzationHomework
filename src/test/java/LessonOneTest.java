bc2352bd4c7baf35c8523f871b14c09236932925
Ilya <ilya.baharev@ext-atom.team>
2025-08-24 18:34:10 +0300

    lesson 1 ex 4

--- src/test/java/LessonOneTest.java
+++ src/test/java/LessonOneTest.java
@@ -1,3 +1,5 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class LessonOneTest {
@@ -5,5 +7,13 @@ public class LessonOneTest {
     @Test
     public void helloFrom() {
         System.out.println("Hello from Ilya");
+    }
+
+    @Test
+    public void testGetRequest() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
