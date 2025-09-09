ece8a09a0bcfb641d8c522353a424f78273b1705
Ilya <ilya.baharev@ext-atom.team>
2025-09-09 15:21:57 +0300

    lesson 3 ex 11

--- src/test/java/LessonThreeTest.java
+++ src/test/java/LessonThreeTest.java
@@ -1,5 +1,8 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LessonThreeTest {
@@ -8,5 +11,17 @@ public class LessonThreeTest {
     public void checkPhraseLength() {
         String hello = "Hello, world";
         assertTrue(hello.length()>15, "The phrase \"" + hello + "\" is too short. Its length is " + hello.length() + " symbols");
+    }
+
+    @Test
+    public void checkCookie() {
+        Response responce = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        String responceCookie = responce.getCookie("HomeWork");
+
+
+        assertEquals("hw_value", responceCookie, "cookie value is unexpected: " + responceCookie);
     }
 }
