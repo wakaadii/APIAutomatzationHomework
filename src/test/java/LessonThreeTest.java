01723c18d562bb24353d1c43a9f030422bb68dd3
Ilya <ilya.baharev@ext-atom.team>
2025-09-09 15:30:57 +0300

    lesson 3 ex 12

--- src/test/java/LessonThreeTest.java
+++ src/test/java/LessonThreeTest.java
@@ -2,6 +2,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -23,5 +24,16 @@ public class LessonThreeTest {
 
 
         assertEquals("hw_value", responceCookie, "cookie value is unexpected: " + responceCookie);
+    }
+
+    @Test
+    public void checkHeader() {
+        Response Responce = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+
+        String responceHeader = Responce.getHeader("x-secret-homework-header");
+
+        assertEquals("Some secret value", responceHeader, "header value is unexpected: " + responceHeader);
     }
 }
