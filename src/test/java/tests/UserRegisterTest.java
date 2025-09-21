80c5e976ad043275f529e23f26ba1baf6859a4a0
Ilya <ilya.baharev@ext-atom.team>
2025-09-21 21:12:16 +0300

    lesson 4 ex 15

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -1,10 +1,9 @@
 package tests;
 
-import io.restassured.RestAssured;
+
 import io.restassured.response.Response;
 import lib.ApiCoreRequests;
 import lib.BaseTestCase;
-import lib.ApiCoreRequests;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -52,9 +51,19 @@ public class UserRegisterTest extends BaseTestCase {
         assertResponseTextEquals(response, "The following required params are missed: " + param);
         assertResponseCodeEquals(response, 400);
     }
-    @Test
-    public void testCreateUserWithShortName() {
-        String name = "a";
+    @ParameterizedTest
+    @ValueSource(strings =  {"a", "Aa1_Bb2-Cc3+Dd4=Ee5|Ff6@Gg7#Hh8$Ii9%Jj0^Kk1&Ll2*Mm3(Nn4)Oo5{Qq6}Rr7[Ss8]Tt9~Uu0`Vv1!Ww2?Xx3.Yy4,Zz5;Aa6_Bb7-Cc8+Dd9=Ee0|Ff1@Gg2#Hh3$Ii4%Jj5^Kk6&Ll7*Mm8(Nn9)Oo0{Qq1}Rr2[Ss3]Tt4~Uu5`Vv6!Ww7?Xx8.Yy9,Zz0Aa1_Bb2-Cc3+Dd4=Ee5|Ff6@Gg7#Hh8$Ii9%Jj0^Kk1&Ll2*Mm3(Nn4)Oo5{Qq6}Rr7[Ss8]Tt9~Uu0`Vv1!Ww2?Xx3.Yy4,Zz5;Aa6_Bb7-Cc8+Dd9=Ee0|Ff1@Gg2#Hh3$Ii4%Jj5^Kk6&Ll7*Mm8(Nn9)Oo0{Qq1}Rr2[Ss3]Tt4~Uu5`Vv6!Ww7?Xx8.Yy9,Zz0"})
+    public void testCreateUserWithSomeNames(String name) {
+
+        String error;
+        if (name.length() < 2){
+            error = "short";
+        } else if (name.length() > 250) {
+            error = "long";
+        } else {
+            error = null;
+        }
+
 
         Map<String, String> userData = new HashMap<>();
         userData.put("firstName", name);
@@ -65,25 +74,7 @@ public class UserRegisterTest extends BaseTestCase {
                 userData
         );
 
-        assertResponseTextEquals(response, "The value of 'firstName' field is too short");
-        assertResponseCodeEquals(response, 400);
-    }
-
-    @Test
-    public void testCreateUserWithLongName() {
-        String name = "Aa1_Bb2-Cc3+Dd4=Ee5|Ff6@Gg7#Hh8$Ii9%Jj0^Kk1&Ll2*Mm3(Nn4)Oo5{Qq6}Rr7[Ss8]Tt9~Uu0`Vv1!Ww2?Xx3.Yy4,Zz5;Aa6_Bb7-Cc8+Dd9=Ee0|Ff1@Gg2#Hh3$Ii4%Jj5^Kk6&Ll7*Mm8(Nn9)Oo0{Qq1}Rr2[Ss3]Tt4~Uu5`Vv6!Ww7?Xx8.Yy9,Zz0Aa1_Bb2-Cc3+Dd4=Ee5|Ff6@Gg7#Hh8$Ii9%Jj0^Kk1&Ll2*Mm3(Nn4)Oo5{Qq6}Rr7[Ss8]Tt9~Uu0`Vv1!Ww2?Xx3.Yy4,Zz5;Aa6_Bb7-Cc8+Dd9=Ee0|Ff1@Gg2#Hh3$Ii4%Jj5^Kk6&Ll7*Mm8(Nn9)Oo0{Qq1}Rr2[Ss3]Tt4~Uu5`Vv6!Ww7?Xx8.Yy9,Zz0";
-
-        Map<String, String> userData = new HashMap<>();
-        userData.put("firstName", name);
-        userData = getRegistrationData(userData);
-
-        Response response = apiCoreRequests.makePostRequest(
-                "https://playground.learnqa.ru/api/user/",
-                userData
-        );
-
-
-        assertResponseTextEquals(response, "The value of 'firstName' field is too long");
+        assertResponseTextEquals(response, "The value of 'firstName' field is too " + error);
         assertResponseCodeEquals(response, 400);
     }
 
